a4cd9f3198c1d474c7f1bf54cdf2139614975591
Ritta <brittanykelly33@gmail.com>
2022-04-25 22:56:50 -0400

    commit tree 7ca6625e26f33916a7664a36c6b1ef8f94f95c86

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity == "bathe my dog") {
+    return `This Saturday, I want to ${activity}!`
+    } else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+}
+
+function mondayWork(work) {
+    switch (work) {
+        case "work from home":
+            return `This Monday, I will ${work}.`
+            break;
+    
+        default:
+            return "This Monday, I will go to the office."
+            break;
+    }
+}
+
+function wrapAdjective(adj = "*") {
+    return function(secretWord = "special"){
+        return `You are ${adj}${secretWord}${adj}!`
+    }
+}
